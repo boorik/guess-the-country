5ae7a9370d5419de7467755f9c6b1cf2d0c1df19
Tifenn Paugois <tfloch@ippon.fr>
2023-10-13 17:17:51 +0200

    initial commit

---
+++ GuessTheCountry/GuessTheCountry/GuessTheCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheCountryApp.swift
+//  GuessTheCountry
+//
+//  Created by Tifenn FLOCH on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
