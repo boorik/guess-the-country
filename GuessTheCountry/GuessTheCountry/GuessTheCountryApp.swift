90c4aeb820333189855e77cacb9a66fcb3f9d6e7
yjacqueline <yjacqueline@ippon.fr>
2023-10-13 16:50:09 +0200

    Initial commit

---
+++ GuessTheCountry/GuessTheCountry/GuessTheCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheCountryApp.swift
+//  GuessTheCountry
+//
+//  Created by Yannick JACQUELINE on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
