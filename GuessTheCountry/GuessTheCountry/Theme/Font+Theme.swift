5b2c0b3ce24549b9186a543a1620462df1433e92
Tifenn Paugois <tfloch@ippon.fr>
2024-03-15 16:19:04 +0100

    feat: add oswald and permanent marker fonts

--- GuessTheCountry/GuessTheCountry/Theme/Font+Theme.swift
+++ GuessTheCountry/GuessTheCountry/Theme/Font+Theme.swift
@@ -3,12 +3,23 @@
 //  GuessTheCountry
 //
 //  Created by Tifenn FLOCH on 03/11/2023.
+//  Checked font names using following
+/* for family in UIFont.familyNames.sorted() {
+    let names = UIFont.fontNames(forFamilyName: family)
+    print("Family: \(family) Font names: \(names)")
+ }
+ */
+/*
+ Family: Oswald Font names: ["Oswald-Regular", "Oswald-Regular_ExtraLight", "Oswald-Regular_Light", "Oswald-Regular_Medium", "Oswald-Regular_SemiBold", "Oswald-Regular_Bold"]
+ Family: Permanent Marker Font names: ["PermanentMarker-Regular"]
+ */
 //
 
 import Foundation
 import SwiftUI
 
 extension Font {
-    static let mainTitle: Font = .largeTitle
-        .bold()
+    static func oswaldRegular(size: CGFloat) -> Font { .custom("Oswald-Regular", size: size) }
+    static func permanentMarker(size: CGFloat) -> Font { .custom("PermanentMarker-Regular", size: size) }
+    static let mainTitle: Font = .permanentMarker(size: 36)
 }
