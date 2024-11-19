d109e02edccf4f3f24279f5f2cfad89630282498
vincent blanchet <vblanchet@ippon.fr>
2024-11-19 10:41:00 +0100

    wip: add didAccept listener

---
+++ GuessTheCountry/GuessTheCountry/Features/Multiplayer/RealTimeGame+GKLocalPlayerListener.swift
@@ -0,0 +1,27 @@
+//
+//  RealTimeGame+GKLocalPlayerListener.swift
+//  GuessTheCountry
+//
+//  Created by vincent blanchet on 11/19/24.
+//
+
+
+import Foundation
+import GameKit
+import SwiftUI
+
+extension RealTimeGame: GKLocalPlayerListener {
+    /// Handles when the local player sends requests to start a match with other players.
+    func player(_ player: GKPlayer, didRequestMatchWithRecipients recipientPlayers: [GKPlayer]) {
+        print("\n\nSending invites to other players.")
+    }
+    
+    /// Presents the matchmaker interface when the local player accepts an invitation from another player.
+    func player(_ player: GKPlayer, didAccept invite: GKInvite) {
+        // Present the matchmaker view controller in the invitation state.
+        if let viewController = GKMatchmakerViewController(invite: invite) {
+            viewController.matchmakerDelegate = self
+            rootViewController?.present(viewController, animated: true) { }
+        }
+    }
+}
